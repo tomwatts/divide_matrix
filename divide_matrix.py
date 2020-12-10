16d1a642f45299a3d5bae9dbe6b487e44a2cc450
Tom <github@tomwatts.ca>
2020-12-10 14:17:42 -0500

    Adding solution and two basic tests.

---
+++ divide_matrix.py
@@ -0,0 +1,33 @@
+def divide_matrix(numbers, size):
+    result = list()
+
+    for i in range(0, len(numbers), size):
+        rows = numbers[i:i + size]
+        for j in range(0, len(numbers), size):
+            submatrix = [r[j:j + size] for r in rows]
+            result.append(submatrix)
+
+    return result
+
+
+matrix = [[0,  1,  2,  3],
+          [4,  5,  6,  7],
+          [8,  9,  10, 11],
+          [12, 13, 14, 15]]
+assert divide_matrix(matrix, 2) == [[[0,  1],  [4,  5]],
+                                    [[2,  3],  [6,  7]],
+                                    [[8,  9],  [12, 13]],
+                                    [[10, 11], [14, 15]]]
+
+
+matrix = [[0,  1,  2,  3,  4,  5],
+          [6,  7,  8,  9,  10, 11],
+          [12, 13, 14, 15, 16, 17],
+          [18, 19, 20, 21, 22, 23],
+          [24, 25, 26, 27, 28, 29],
+          [30, 31, 32, 33, 34, 35]]
+assert divide_matrix(matrix, 3) == [[[0,  1,  2],  [6,  7,  8],  [12, 13, 14]],
+                                    [[3,  4,  5],  [9,  10, 11], [15, 16, 17]],
+                                    [[18, 19, 20], [24, 25, 26], [30, 31, 32]],
+                                    [[21, 22, 23], [27, 28, 29], [33, 34, 35]]]
+
